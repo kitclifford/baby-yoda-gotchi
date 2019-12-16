979a2b3055d2641047668bb55f16c46cf6e32977
Gareth Dobson <garethdobson@gmx.com>
2019-12-16 16:43:07 +0000

    First draft of game page styling

--- baby-yoda/src/components/Game.js
+++ baby-yoda/src/components/Game.js
@@ -1,9 +1,27 @@
 import React from "react";
+import { ProgressBar } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
 import '../yoda.css';
 
 const Game = () => (
      <React.Fragment>
-          <div className="baby-yoda"></div>
+          <div className="content">
+               <h1>Baby Micheal</h1>
+               <div className="gamebox">
+                    <div className="baby-yoda"></div>
+               </div>
+               <div className="game-controls">
+                    <h2>Age: 52</h2>
+               </div>
+               <div className="game-controls">
+                    <p>Health: </p>
+                    <ProgressBar className="m-3" variant="danger" animated now={70} />
+                    <Button className="m-3" variant="outline-warning">Feed Baby Micheal</Button>
+               </div>
+               <div className="game-controls">
+                    <Button className="m-3" variant="outline-danger">Terminate</Button>
+               </div>
+          </div>
      </React.Fragment>
 );
 
