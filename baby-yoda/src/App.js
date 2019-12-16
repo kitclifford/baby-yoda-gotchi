08ebdaf634ccb26e265e84db2d4ae10fbb4ec726
Gareth Dobson <garethdobson@gmx.com>
2019-12-16 11:35:10 +0000

    Installed react bootstrap

--- baby-yoda/src/App.js
+++ baby-yoda/src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        BABY YODA APP
+        <h1>BABY-YODA-GOTCHI</h1>
       </header>
     </div>
   );
