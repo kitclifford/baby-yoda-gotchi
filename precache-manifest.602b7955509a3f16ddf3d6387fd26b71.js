ac3d7f332e78ea8ceb17b173921448ee792a8c5b
Kit Clifford-Page <kit.clifford.page@gmail.com>
2019-12-20 15:09:37 +0000

    Updates

---
+++ precache-manifest.602b7955509a3f16ddf3d6387fd26b71.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bd2ccf48417481628a35268b8a03623",
+    "url": "/baby-yoda-gotchi/index.html"
+  },
+  {
+    "revision": "7564ae0515a6c36e5da8",
+    "url": "/baby-yoda-gotchi/static/css/main.232f5417.chunk.css"
+  },
+  {
+    "revision": "8172ff223f5f7ac35de2",
+    "url": "/baby-yoda-gotchi/static/js/2.e279a4d6.chunk.js"
+  },
+  {
+    "revision": "87984cad984dae3222b2a1d0861003a4",
+    "url": "/baby-yoda-gotchi/static/js/2.e279a4d6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7564ae0515a6c36e5da8",
+    "url": "/baby-yoda-gotchi/static/js/main.1549c6d4.chunk.js"
+  },
+  {
+    "revision": "7cec26be062bdc2b8f06",
+    "url": "/baby-yoda-gotchi/static/js/runtime-main.5fdcfd4e.js"
+  },
+  {
+    "revision": "1d06559b014a5ce5d1352429b60e3b37",
+    "url": "/baby-yoda-gotchi/static/media/space_background.1d06559b.jpg"
+  },
+  {
+    "revision": "3fa8d3cb0e3f16c3818607449ef59f70",
+    "url": "/baby-yoda-gotchi/static/media/yodaNoise.3fa8d3cb.mp3"
+  }
+]);
